f02c3bcff0d7295460ec9a3810fd18a5f83fc5c3
MoldovanPaul11 <moldovanpaul2000@gmail.com>
2026-02-19 17:57:21 +0200

    .

---
+++ Gestiune_Service/Gestiune_Service/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Gestiune_Service.model;
+namespace Gestiune_Service
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Car> CarsInService { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            CarsInService = new ObservableCollection<Car>();
+
+            CarsInService.Add(new Car("Dacia", "logan", "BV05abc", "pana", 100, false));
+
+
+            this.DataContext = this;
+        }
+
+        private void btnAddCar_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtmarca.Text))
+            {
+                MessageBox.Show("Te rog introdu marca masinii");
+                return;
+            }
+            else if (string.IsNullOrWhiteSpace(txtmodel.Text))
+            {
+                MessageBox.Show("Te rog introdu modelul masinii");
+            }
+
+            Car newCar = new Car(txtmarca.Text, txtmodel.Text, txtNrInmatriculare.Text, "urmeaza constatare", 0, false);
+
+            CarsInService.Add(newCar);
+
+            txtmarca.Clear();
+            txtmodel.Clear();
+            txtNrInmatriculare.Clear();
+
+            txtmarca.Focus();
+
+        }
+        private void btnDeleteCar_Click(object sender, RoutedEventArgs e)
+        {
+            Button buttonpressed = sender as Button;
+
+            Car carForDelete = buttonpressed.DataContext as Car;
+
+            if (carForDelete != null)
+            {
+                CarsInService.Remove(carForDelete);
+            }
+        }
+    }
+}
---
+++ Gestiune_Service/Gestiune_Service/model/Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestiune_Service.model
+{
+    public class Car
+    {
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public string NrInmatriculare {  get; set; }
+        public string Problem {  get; set; }
+        public double Price {  get; set; }
+        public bool Finished {  get; set; }
+
+        public Car()
+        {
+            Name = string.Empty;
+            Model = string.Empty;
+            NrInmatriculare= string.Empty;
+            Problem=string.Empty;
+            Price=0;
+            Finished = false;
+        }
+        public Car(string name, string model, string nrinmatriculare, string problem, double price, bool finished)
+        {
+            Name = name;
+            Model = model;
+            NrInmatriculare = nrinmatriculare;
+            Problem = problem;
+            Price = price;
+            Finished = finished;
+        }
+    }
+}
